0a6c50a4fd31ae32b64171d9b1bb5277f34b597c
unknown <Anthony@ANT-LAPTOP.RA.local>
2017-07-15 12:52:49 +0100

    Creating BookShelf component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as BooksAPI from './BooksAPI';
 import './App.css';
 import BookShelfChanger from './Components/BookShelfChanger';
 import Book from './Components/Book';
+import BookShelf from './Components/BookShelf' 
 
 class BooksApp extends React.Component {
   state = {
@@ -41,43 +42,7 @@ class BooksApp extends React.Component {
               </div>
               <div className="list-books-content">
                 <div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-                        <li>
-                          <Book
-                            bookData={{
-                              BookTitle: 'To Kill a Mockingbird',
-                              Authors: 'Harper Lee',
-                              Image:
-                                'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api',
-                              Width: 128,
-                              Height: 188
-                            }}
-                          />
-                        </li>
-                        <li>
-                          <div className="book">
-                            <div className="book-top">
-                              <div
-                                className="book-cover"
-                                style={{
-                                  width: 128,
-                                  height: 188,
-                                  backgroundImage:
-                                    'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
-                                }}
-                              />
-                              <BookShelfChanger />
-                            </div>
-                            <div className="book-title">Ender's Game</div>
-                            <div className="book-authors">Orson Scott Card</div>
-                          </div>
-                        </li>
-                      </ol>
-                    </div>
-                  </div>
+                  <BookShelf />
                   <div className="bookshelf">
                     <h2 className="bookshelf-title">Want to Read</h2>
                     <div className="bookshelf-books">
