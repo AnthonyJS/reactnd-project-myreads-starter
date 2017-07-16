0e4582c29accb8353d0b46a60389e81cbc123cfc
Anthony <ajanthon@gmail.com>
2017-07-16 14:12:02 +0100

    Bugfix to ensure selection in drop down list updates on selection change

--- src/Components/BookShelfChanger.js
+++ src/Components/BookShelfChanger.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const BookShelfChanger = props => {
-  return (
-    <div className="book-shelf-changer">
-      <select value={props.shelf} onChange={event => props.amendShelfHandler(props.book, event.target.value)}>
-        <option value="none" disabled>
-          Move to...
-        </option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="read">Read</option>
-        <option value="none">None</option>
-      </select>
-    </div>
-  )
+class BookShelfChanger extends Component {
+  state = {
+    shelf: this.props.shelf
+  }
+
+  /**
+  * @description Ensures when selection changes, that the selection in the drop down list also changes
+  * @param {event} event
+  * @returns void
+  */
+  changeHandler = event => {
+    this.setState({ shelf: event.target.value })
+    this.props.amendShelfHandler(this.props.book, event.target.value)
+  }
+
+  render() {
+    return (
+      <div className="book-shelf-changer">
+        <select value={this.state.shelf} onChange={event => this.changeHandler(event)}>
+          <option value="none" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    )
+  }
 }
 
 BookShelfChanger.propTypes = {
